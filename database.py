e46c33ab4571552f9a12b6b959ecd22499d036a7
Wooft <wooftyaa@gmail.com>
2023-08-31 23:14:47 +0300

    add database settings

---
+++ database.py
@@ -0,0 +1,25 @@
+import atexit
+from sqlalchemy import Column, String, Integer, create_engine, Numeric
+from sqlalchemy.orm import create_session
+from sqlalchemy.ext.declarative import declarative_base
+from decouple import config
+
+PG_DSN = f'postgresql://{config("DB_USER")}:{config("DB_PASSWORD")}@127.0.0.1:5432/{config("DB_NAME")}'
+engine = create_engine(PG_DSN)
+
+Base = declarative_base()
+Session = create_session(bind=engine)
+
+atexit.register(engine.dispose)
+class Parts(Base):
+    __tablename__ = 'parts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    code = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
+    price = Column(Numeric, nullable=True)
+
+    def __str__(self):
+        return self.code
+
+Base.metadata.create_all(bind=engine)
